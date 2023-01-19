26a2d094fec10427c767ae84ff7d88857fd4b9bc
Tr3ndyyute <101469816+Tr3ndyyute@users.noreply.github.com>
2023-01-19 18:25:02 +0100

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-const arg = process.argv[2];
-if (arg === undefined) {
-  console.log('No argument');
+
+// Script that prints "JavaScript is amazing".
+
+if (process.argv[2]) {
+  console.log(process.argv[2]);
 } else {
-  console.log(arg);
+  console.log('No argument');
 }
