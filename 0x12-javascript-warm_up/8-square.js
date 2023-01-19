919fc18d78328b1f7747c376d918ee6e660871e0
Tr3ndyyute <101469816+Tr3ndyyute@users.noreply.github.com>
2023-01-19 18:28:34 +0100

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,17 @@
 #!/usr/bin/node
-const x = process.argv[2];
-if (isNaN(x)) {
+
+// Script to print square of size argv[2].
+
+const myArg = process.argv[2];
+let rows = 'X';
+
+if (isNaN(Number(myArg))) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < x; i++) {
-    console.log('X'.repeat(x));
+  for (let i = 0; i < myArg - 1; i++) {
+    rows += 'X';
+  }
+  for (let j = 0; j < myArg; j++) {
+    console.log(rows);
   }
 }
