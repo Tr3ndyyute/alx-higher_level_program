5354d0ef3aeed19fae1566ee7820a175aa530e50
Tr3ndyyute <101469816+Tr3ndyyute@users.noreply.github.com>
2023-01-21 11:07:12 +0100

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-""" post email """
+"""
+    Script that takes in a URL and an email sends a POST request to that URL
+    with the email as a parameter.
+    Displays the body of the response
+"""
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    email = {'email': sys.argv[2]}
-    r = requests.post(sys.argv[1], data=email)
-    print(r.text)
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print("{}".format(r.text))
