37b19b2c6398ad4fb1686514a804bdf2f3112581
Tr3ndyyute <101469816+Tr3ndyyute@users.noreply.github.com>
2023-01-21 11:00:04 +0100

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-""" send POST request to github """
+"""
+    Script that takes GitHub Credentials (username and password)
+    used GitHub API to display your id
+"""
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    response = requests.get("https://api.github.com/user", auth=(
-        sys.argv[1], sys.argv[2]))
-    result = response.json()
-    print(result.get("id"))
+    url = 'https://api.github.com/user'
+    r = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
+    r_json = r.json()
+    if r_json == {}:
+        print("None")
+    else:
+        print("{}".format(r_json.get('id')))
